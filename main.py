aeb073ad41a1656fa8825e26c97743ea13421fa6
Lucas <you@example.com>
2024-03-17 22:26:10 +0100

    created correlation graph with two dropdowns and footer

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import plotly_express as px
 import dash_bootstrap_components as dbc
 from ChangeGraphs import ChangeGraphs
 from dataScripts.stockMarketVisualization import StockMarketData
+from correlationGraph import CorrelationGraph
 import plotly.io as pio
 
 
@@ -52,6 +53,7 @@ app.layout=dbc.Container(children=[
         ],
         style={"margin-bottom": "20px"}
     ),
+        
         dbc.Row([
               dbc.Button("Change graph", id="change_graph_button",color=colors['bg'], className="m-4 me-1 border border-white rounded ",style={'color': colors['text'],'width':'120px', 'font-size':'13px'},n_clicks=0),
         ],className="w-100 d-flex flex-row-reverse"),
@@ -59,14 +61,70 @@ app.layout=dbc.Container(children=[
                dcc.Graph( id='graph',figure={"layout":{
                    "template":"plotly_dark"
                }})
-        ])
+        ]),
+    
+        dbc.Row([
+        html.Hr(
+            style={
+                'color': colors['text'],
+                'margin-top':'20px'
+            }
+        )
+        ]),
+        dbc.Row([
+            html.Div(
+            children='Correlation beetwen companies',
+            className="font-weight-bold text-center display-6 p-3 pt-5",
+            style={
+                'color': colors['text'],
+                'background-color': colors['bg']
+            }
+                )
+        ]),
+        dbc.Row([
+            dbc.Col(
+            dcc.Dropdown(
+                df_tickers["Symbol"] + " - " + df_tickers["FullName"],
+                id='correlation_dropdown',
+                value='MSFT - "Microsoft Corporation"',
+                className='pt-5 w-25 mx-auto',
+                style={'background-color': colors['bg'],'color': colors["text2"]}
+            )
+            ),
+            dbc.Col(
+            dcc.Dropdown(
+                df_tickers["Symbol"] + " - " + df_tickers["FullName"],
+                id='correlation2_dropdown',
+                value='AAPL - "Apple, Inc."',
+                className='pt-5 w-25 mx-auto',
+                style={'background-color': colors['bg'],'color': colors["text2"]}
+            )
+            )
+        ],
+        style={"margin-bottom": "20px"}
+    ),
+     dbc.Row([
+               dcc.Graph( id='corr_graph',figure={"layout":{
+                   "template":"plotly_dark"
+               }})
+        ]),
+         dbc.Row(
+            dbc.Col(
+            html.P(
+                f"\u00A9 All rights reserved. Made by Kacper Potaczała and Łukasz Cal.",
+                className="text-end text-white pt-3",
+            ),
+            width={"size": 12},
+            style={"background-color": "#2E3135"}
+            )
+        )
 
 
 ],fluid=True,style={'background-color': colors['bg'],'overflow': 'auto','height':'100vh'})
 @callback(
     Output(component_id='graph',component_property='figure'),
     Input(component_id='dropdown',component_property='value'),
-    Input(component_id='change_graph_button',component_property='n_clicks'),
+    Input(component_id='change_graph_button',component_property='n_clicks')
 )
 
 def update_graph(company_choosen : str,n):
@@ -76,7 +134,7 @@ def update_graph(company_choosen : str,n):
     cg = ChangeGraphs(df_tickers)
     company_fetched = market_object.get_company(company_choosen.split("-")[0])
     textSplited = company_choosen.split("-")
-    companyName = f"<br>{textSplited[0]} - ({textSplited[1].replace("\"","").strip()})"
+    companyName = f"<br>{textSplited[0]} - ({textSplited[1].strip()})"
     global c
     if ctx.triggered_id=='change_graph_button': c = c+1
     if c>2:
@@ -96,6 +154,20 @@ def update_graph(company_choosen : str,n):
     elif c==2:
         figure = cg.changeGraphToHistogram(company_fetched,companyName)
         return figure
+    
+@callback(
+    Output(component_id='corr_graph',component_property='figure'),
+    Input(component_id='correlation_dropdown',component_property='value'),
+    Input(component_id='correlation2_dropdown',component_property='value')
+)
+def getCorrelationGrap(company1,company2):
+    company_fetched1 = market_object.get_company(company1.split("-")[0])
+    textSplited1 = company1.split("-")
+    company_fetched2 = market_object.get_company(company2.split("-")[0])      
+    textSplited2 = company2.split("-")                                     
+    corrg = CorrelationGraph(df_tickers)
+    fig = corrg.handleCorrelation(company_fetched1,company_fetched2,textSplited1,textSplited2)
+    return fig
     
 
 if __name__ == '__main__':
