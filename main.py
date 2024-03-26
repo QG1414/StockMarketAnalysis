ccee6d048c941429fbe01ad409e78d0675ce7960
Kacper Potaczała <Kacperpotaczala001@gmail.com>
2024-03-26 16:34:58 +0100

    Finished creating classes and adding save breaks

--- main.py
+++ main.py
@@ -3,23 +3,34 @@ import dash_bootstrap_components as dbc
 from dataScripts.stockMarketVisualization import StockMarketData
 import plotly.io as pio
 from layout import Layout
-from callbacks import get_callbacks
+from callbacks import Callbacks
 
 pio.templates.default = "plotly_dark"
 external_stylesheets = [dbc.themes.CERULEAN]
 
-app=Dash(__name__, external_stylesheets=external_stylesheets)
+class MainApp:
+    def __init__(self) -> None:
+        self.__app=Dash(__name__, external_stylesheets=external_stylesheets)
 
-market_object = StockMarketData()
-df_tickers = market_object.get_tickets()
+        self.__market_object = StockMarketData()
+        self.__df_tickers = self.__market_object.get_tickets()
+        self.__layout_data = Layout(self.__df_tickers)
 
-layout_data = Layout(df_tickers)
+        self.__callbacks = Callbacks()
+        self.__callbacks.get_callbacks(self.__app,self.__layout_data)
 
-app.layout=layout_data.content_layout
-
-get_callbacks(app,df_tickers)
+        self.__app.layout=self.__on_page_reload
 
 
+    def run_app(self) -> None:
+        self.__app.run(debug=True)
+
+    def __on_page_reload(self):
+        self.__callbacks.reset_values()
+        return self.__layout_data.content_layout
+
+
+app = MainApp()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_app()
