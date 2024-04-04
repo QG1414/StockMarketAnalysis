7bc2afd33b209ceea4f9d98915863d2622b81655
Kacper Potaczała <Kacperpotaczala001@gmail.com>
2024-04-04 20:02:54 +0200

    added ticket file from url

--- dataScripts/stockMarketVisualization.py
+++ dataScripts/stockMarketVisualization.py
@@ -7,33 +7,30 @@ import datetime
 
 class StockMarketData:
     def __init__(self) -> None:
-        self.__tickets_file_name = AbsolutePath.resource_path("dataScripts\\data\\YahooTickers.csv")
-        self.__base_data_name = AbsolutePath.resource_path("dataScripts\\data\\nasdaq_screener.csv")
-
-        if(not os.path.isfile(self.__tickets_file_name)):
-            self.__create_tickets_file()
+        __tickets_url = "https://drive.google.com/file/d/1sl0pmPgA3FISpp28KoYqqiFfG46DvMVX/view?usp=sharing"
+        __tickets_url = 'https://drive.google.com/uc?id=' + __tickets_url.split('/')[-2]
             
-        self.__tickers_data = pd.read_csv(self.__tickets_file_name)
+        self.__tickers_data = pd.read_csv(__tickets_url)
         self.__tickers_data["Symbol"] = self.__tickers_data["Symbol"].astype(str)
 
-    def __create_tickets_file(self) -> None:
-        screener_data = pd.read_csv(self.__base_data_name)
-        screener_data["Symbol"] = screener_data["Symbol"].astype(str)
+    # def __create_tickets_file(self) -> None:
+    #     screener_data = pd.read_csv(self.__base_data_name)
+    #     screener_data["Symbol"] = screener_data["Symbol"].astype(str)
 
-        final_data = {"Symbol":[],"FullName":[]}
+    #     final_data = {"Symbol":[],"FullName":[]}
 
-        for i in screener_data["Symbol"].values:
-            try:
-                stock = yf.Ticker(i)
-                longName = stock.info['longName']
-                final_data["Symbol"] += [i]
-                final_data["FullName"] += [longName]
-            except:
-                continue
+    #     for i in screener_data["Symbol"].values:
+    #         try:
+    #             stock = yf.Ticker(i)
+    #             longName = stock.info['longName']
+    #             final_data["Symbol"] += [i]
+    #             final_data["FullName"] += [longName]
+    #         except:
+    #             continue
 
-        data_frame_to_save = pd.DataFrame(final_data)
-        data_frame_to_save.reset_index(inplace=True, drop=True)
-        data_frame_to_save.to_csv(self.__tickets_file_name, encoding="utf-8", index=False)
+    #     data_frame_to_save = pd.DataFrame(final_data)
+    #     data_frame_to_save.reset_index(inplace=True, drop=True)
+    #     data_frame_to_save.to_csv(self.__tickets_file_name, encoding="utf-8", index=False)
     
     def get_tickets(self) -> pd.DataFrame:
         return self.__tickers_data
